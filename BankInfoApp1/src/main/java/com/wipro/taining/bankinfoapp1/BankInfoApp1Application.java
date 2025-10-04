9053e914f35a2adfe193cfad67be52639cc766ab
Vishwarekha Ravichandran <53889187+vishwarekha@users.noreply.github.com>
2025-10-04 22:55:09 +0530

    Add files via upload
    BankInfoApp1

---
+++ BankInfoApp1/src/main/java/com/wipro/taining/bankinfoapp1/BankInfoApp1Application.java
@@ -0,0 +1,13 @@
+package com.wipro.taining.bankinfoapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankInfoApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankInfoApp1Application.class, args);
+	}
+
+}
